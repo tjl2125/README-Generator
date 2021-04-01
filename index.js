2a5ead57548d616283659387a7afa701907851b5
Taylor Leong <tjl2125@alum.barnard.edu>
2021-03-31 20:44:41 -0400

    laying down the skeleton work

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const questions = require("./questions.js");
+const inquirer = require("inquirer");
+const generateHTML = (answers) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>HTML-generator</title>
+</head>
+<body>
+    <div class="jumbotron">
+        <div class="container">
+            <!-- Name -->
+            <h1>Hi my name is ${answers.name}</h1>
+            <!-- location -->
+            <h3>I am from ${answers.location}</h3>
+            <!-- Bio -->
+            <h3>A little bit about me: ${answers.bio}</h3>
+            <!-- Linkedin url -->
+            <p>My Linkedin is: ${answers.linkedin}</p>
+            <!-- Github username -->
+            <p>My Github is:${answers.github}</p>
+        </div>
+    </div>
+</body>
+</html>`;
+inquirer
+    .prompt(questions)
+    .then((data) => {
+        const filename = `${data.name.toLowerCase().split(' ').join('')}.md`;
+        fs.writeFile(filename, generateHTML(data), (err) =>
+          err ? console.log(err) : console.log('Success!')
+        );
+      });
+
+
+
+
+
---
+++ questions.js
@@ -0,0 +1,27 @@
+module.exports = [
+	{
+		type: 'input',
+		name: 'name',
+		message: 'What is your name?',
+	},
+	{
+		type: 'input',
+		name: 'location',
+		message: 'What is your location?',
+	},
+	{
+		type: 'input',
+		name: 'bio',
+		message: 'Share a little bit about yourself.',
+	},
+  {
+    type: 'input',
+    name: 'linkedin',
+    message: 'What is your linkedin URL?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your Github URL?',
+  },
+];
